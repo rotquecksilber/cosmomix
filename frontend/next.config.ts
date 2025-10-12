56ec786d5d850ad36a69a7f6e89ea3263d99a949
Valentin <prefectzao@gmail.com>
2025-10-12 19:08:54 +0400

    pre-production

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  images: {
-    domains: ['asfarylab.com', 'koreatrade.ru'], // разрешаем внешние домены
-  },
+
 };
 
 export default nextConfig;
