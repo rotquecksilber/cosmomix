9d17f7ae66c54b6ec3792b6bd2f7c001e5da5613
Valentin <prefectzao@gmail.com>
2025-11-20 19:08:15 +0400

    sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,4 +2,20 @@
 module.exports = {
   siteUrl: 'https://cosmo-mix.ru',
   generateRobotsTxt: true,
+
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: [
+          '/_next/',
+          '/api/',
+          '/manifest.json',
+          '/service-worker.js',
+          '/sw.js',
+        ],
+      },
+    ],
+  },
 };
